56d826d66492afbabb12401634975861099de209
Husain <husaincementwalas@gmail.com>
2025-01-24 17:11:40 +0530

    move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,25 @@
 export const cart = [];
+
+//this function is called on clicking add to cart in amazon.js file
+export function addToCart(productId){
+
+  let matchingItem;
+  cart.forEach((cartItem)=>{
+    if(productId === cartItem.productId){ 
+      matchingItem = cartItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+  const quantitySelect = Number(quantitySelector.value);
+   
+  if(matchingItem){ //it is an object of truthy value
+   matchingItem.quantity += quantitySelect;
+  }
+  else{
+    cart.push({
+    productId : productId,
+    quantity : quantitySelect  
+   });
+  }
+}
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,8 +1,9 @@
 //this file gets all the products from data/products.js file and then it starts running
 
 
-import { cart } from '../data/cart.js';
-import { products } from '../data/products.js'
+import {cart, addToCart} from '../data/cart.js';
+import {products} from '../data/products.js'
+
 // '../' gets us out of current folder
 
 
@@ -72,69 +73,23 @@ console.log(productsHTML);
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
-const addedMessageTimeouts = {}; //for he greem pop message
+const addedMessageTimeouts = {}; //for the green pop message
 
 
-//on clicking the add-to-cart button
-document.querySelectorAll('.js-add-to-cart')
-.forEach((button)=>{
+function updateCartQuantity(productId){
 
-  button.addEventListener('click',()=>{
- 
-   const productId = button.dataset.productId;
-   //or we can write = const {product} = button.dataset;
-
-
-   // the product-id on line 54 gets converted from kebab-case to camelCase.
-   //using Id instead of name because 2 products can have the same name but always a unique id
-
-
-   
-  let matchingItem;
-
-  cart.forEach((item)=>{
-    if(productId === item.productId){
-    
-      matchingItem = item;
-
-    }
-  });
-
-  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-
-  const quantitySelect = Number(quantitySelector.value);
-   
-  if(matchingItem){ //it is an object of truthy value
-
-   matchingItem.quantity += quantitySelect;
-  }
-
-  else{
-    cart.push({
-    productId : productId,
-    quantity : quantitySelect
-    
-   });
-  }
-
-//Calculating total quantity in the cart for the shopping cart icon on right side
+  //Calculating total quantity in the cart for the shopping cart icon on right side
   let cartQuantity =0;
 
-  cart.forEach((item)=>{
-cartQuantity += item.quantity;
+  cart.forEach((cartItem)=>{
+cartQuantity += cartItem.quantity;
   });
-
-
   document.querySelector('.js-cart-quantity')
   .innerHTML = cartQuantity; //puttin the cart value on the icon
 
-
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
   addedMessage.classList.add('added-to-cart-visible');
 //here we will display the green message using css style where we turn the opacity of message from 0 to 1 for 2 seconds
-
-
 
   // Check if there's a previous timeout for this
   // product. If there is, we should stop it.
@@ -142,7 +97,6 @@ cartQuantity += item.quantity;
   if (previousTimeoutId) {
     clearTimeout(previousTimeoutId);
   }
-
   const timeoutId = setTimeout(() => {
     addedMessage.classList.remove('added-to-cart-visible');
   }, 2000);
@@ -150,15 +104,30 @@ cartQuantity += item.quantity;
   // Save the timeoutId for this product
   // so we can stop it later if we need to.
   addedMessageTimeouts[productId] = timeoutId;
+  // the product-id  gets converted from kebab-case to camelCase.
+//using Id instead of name because 2 products can have the same name but always a unique id
+}
 
 
 
+/*********************************** */
+//on clicking the add-to-cart button
+document.querySelectorAll('.js-add-to-cart')
+.forEach((button)=>{
 
-  /*
+  button.addEventListener('click',()=>{
+  const productId = button.dataset.productId;
+  //or we can write = const {product} = button.dataset;
+
+addToCart(productId); //since productId was defined outside funtion we pass it as parameter
+updateCartQuantity(productId);
+
+ });
+});
+
+/*
   Steps:
   1. attached product-name using data attribute (in notes).
   2. on clicking the button we got the productName out.
   3. we add product to the cart (in file data/cart.js).
   */
- });
-});
